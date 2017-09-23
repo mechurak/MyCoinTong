742057de1f39d340c01b6cd5a9182dd63096926b
sshim from desktop <mechurak@gmail.com>
2017-09-23 12:53:34 +0900

    Add footer to listView to show portfolio

--- app/src/main/java/com/shimnssso/mycointong/MainActivity.java
+++ app/src/main/java/com/shimnssso/mycointong/MainActivity.java
@@ -12,7 +12,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -27,6 +29,7 @@ import com.shimnssso.mycointong.network.KorbitClient;
 import com.shimnssso.mycointong.network.TickerListener;
 import com.shimnssso.mycointong.setting.SettingActivity;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener, ListView.OnItemLongClickListener, TickerListener {
@@ -36,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
 
     private SwipeRefreshLayout mSwipeLayout;
     private AdView mAdView;
+    private View mFooter;
+    DecimalFormat mIntFormatter = new DecimalFormat("#,###");
+    DecimalFormat mFloatFormatter = new DecimalFormat("#,##0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         }
 
         ListView listView = (ListView) findViewById(R.id.listView);
+        mFooter = getLayoutInflater().inflate(R.layout.listview_footer, null, false);
+        listView.addFooterView(mFooter);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
         listView.setOnItemLongClickListener(this);
@@ -175,11 +183,13 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
                     item.setMyQuantity(quantity);
 
                     adapter.notifyDataSetChanged();
+                    updateRevenue();
                 }
                 break;
             case Const.RequestCode.SettingActivity:
                 if (resultCode == RESULT_OK) {
                     updateAdapterFromDb();
+                    updateRevenue();
                 }
                 break;
             default:
@@ -227,7 +237,10 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
                 boolean removeRet = mRefreshList.remove(site);
                 Log.d(TAG, "removeRet: " + removeRet);
 
-                if (mRefreshList.isEmpty()) mSwipeLayout.setRefreshing(false);
+                if (mRefreshList.isEmpty()) {
+                    mSwipeLayout.setRefreshing(false);
+                    updateRevenue();
+                }
             }
         }
     }
@@ -273,5 +286,150 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
             adapter.addItem(item);
         }
         adapter.notifyDataSetChanged();
+    }
+
+    private void updateRevenue() {
+        ListViewAdapter adapter = ListViewAdapter.getInstance();
+        double[] initialPrice = {0.0d, 0.0d, 0.0d, 0.0d};  // KRW, USD, CNY, JPY
+        double[] currentPrice = {0.0d, 0.0d, 0.0d, 0.0d};
+        double curPrice, curInitPrice;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            ListViewItem curItem = (ListViewItem)adapter.getItem(i);
+            double myQuantity = curItem.getMyQuantity();
+            if (myQuantity > 0.0d) {
+                Log.d(TAG, "updateRevenue(). plus "  + curItem.getFullName());
+                curPrice = curItem.getCurPrice() * curItem.getMyQuantity();
+                curInitPrice = curItem.getMyAvgPrice() * curItem.getMyQuantity();
+                String currency = curItem.getCurrency();
+                switch (currency) {
+                    case Const.Currency.KRW:
+                        currentPrice[0] += curPrice;
+                        initialPrice[0] += curInitPrice;
+                        break;
+                    case Const.Currency.USD:
+                        currentPrice[1] += curPrice;
+                        initialPrice[1] += curInitPrice;
+                        break;
+                    case Const.Currency.CNY:
+                        currentPrice[2] += curPrice;
+                        initialPrice[2] += curInitPrice;
+                        break;
+                    case Const.Currency.JPY:
+                        currentPrice[3] += curPrice;
+                        initialPrice[3] += curInitPrice;
+                        break;
+                    default:
+                        Log.w(TAG, "updateRevenue(). unexpected currency. " + currency);
+                        break;
+                }
+            }
+        }
+        if (initialPrice[0] == 0.0d && initialPrice[1] == 0.0d && initialPrice[2] == 0.0d && initialPrice[3] == 0.0d) {
+            mFooter.setVisibility(View.GONE);
+        } else {
+            mFooter.setVisibility(View.VISIBLE);
+            LinearLayout layoutKRW = (LinearLayout) mFooter.findViewById(R.id.layoutKRW);
+            LinearLayout layoutUSD = (LinearLayout) mFooter.findViewById(R.id.layoutUSD);
+            LinearLayout layoutCNY = (LinearLayout) mFooter.findViewById(R.id.layoutCNY);
+            LinearLayout layoutJPY = (LinearLayout) mFooter.findViewById(R.id.layoutJPY);
+            layoutKRW.setVisibility(View.GONE);
+            layoutUSD.setVisibility(View.GONE);
+            layoutCNY.setVisibility(View.GONE);
+            layoutJPY.setVisibility(View.GONE);
+
+            if (initialPrice[0] != 0.0d) {
+                layoutKRW.setVisibility(View.VISIBLE);
+                fillFooterForCurrecy(0, initialPrice, currentPrice, true);
+            }
+            if (initialPrice[1] != 0.0d) {
+                layoutUSD.setVisibility(View.VISIBLE);
+                fillFooterForCurrecy(1, initialPrice, currentPrice, false);
+            }
+            if (initialPrice[2] != 0.0d) {
+                layoutCNY.setVisibility(View.VISIBLE);
+                fillFooterForCurrecy(2, initialPrice, currentPrice, false);
+            }
+            if (initialPrice[3] != 0.0d) {
+                layoutJPY.setVisibility(View.VISIBLE);
+                fillFooterForCurrecy(3, initialPrice, currentPrice, false);
+            }
+        }
+    }
+
+    private void fillFooterForCurrecy(int index, double[] initialPrice, double[] currentPrice, boolean useIntCurrency) {
+        if (index < 0 || index > 3) {
+            Log.e(TAG, "fillFooterForCurrecy(). unexpected index. " + index);
+            return;
+        }
+        TextView txt_revenuePercent = (TextView) mFooter.findViewById(R.id.txt_revenuePercentKRW);
+        TextView txt_revenuePrice = (TextView) mFooter.findViewById(R.id.txt_revenuePriceKRW);
+        TextView txt_currentPrice = (TextView) mFooter.findViewById(R.id.txt_currentPriceKRW);
+        TextView txt_initialPrice = (TextView) mFooter.findViewById(R.id.txt_initialPriceKRW);
+        String currencySymbol = "₩";
+        switch (index) {
+            case 0:
+                // Do nothing. Proper views are already selected
+                break;
+            case 1:
+                txt_revenuePercent = (TextView) mFooter.findViewById(R.id.txt_revenuePercentUSD);
+                txt_revenuePrice = (TextView) mFooter.findViewById(R.id.txt_revenuePriceUSD);
+                txt_currentPrice = (TextView) mFooter.findViewById(R.id.txt_currentPriceUSD);
+                txt_initialPrice = (TextView) mFooter.findViewById(R.id.txt_initialPriceUSD);
+                currencySymbol = "$";
+                break;
+            case 2:
+                txt_revenuePercent = (TextView) mFooter.findViewById(R.id.txt_revenuePercentCNY);
+                txt_revenuePrice = (TextView) mFooter.findViewById(R.id.txt_revenuePriceCNY);
+                txt_currentPrice = (TextView) mFooter.findViewById(R.id.txt_currentPriceCNY);
+                txt_initialPrice = (TextView) mFooter.findViewById(R.id.txt_initialPriceCNY);
+                currencySymbol = "Ұ";
+                break;
+            case 3:
+                txt_revenuePercent = (TextView) mFooter.findViewById(R.id.txt_revenuePercentJPY);
+                txt_revenuePrice = (TextView) mFooter.findViewById(R.id.txt_revenuePriceJPY);
+                txt_currentPrice = (TextView) mFooter.findViewById(R.id.txt_currentPriceJPY);
+                txt_initialPrice = (TextView) mFooter.findViewById(R.id.txt_initialPriceJPY);
+                currencySymbol = "¥";
+                break;
+            default:
+                break;
+        }
+
+        String priceText;
+        if (useIntCurrency) {
+            priceText = mIntFormatter.format(initialPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_initialPrice.setText(priceText);
+
+            priceText = mIntFormatter.format(currentPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_currentPrice.setText(priceText);
+
+            priceText = mIntFormatter.format(currentPrice[index] - initialPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_revenuePrice.setText(priceText);
+        } else {
+            priceText = mFloatFormatter.format(initialPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_initialPrice.setText(priceText);
+
+            priceText = mFloatFormatter.format(currentPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_currentPrice.setText(priceText);
+
+            priceText = mFloatFormatter.format(currentPrice[index] - initialPrice[index]);
+            priceText = Util.priceWithSymbol(currencySymbol, priceText);
+            txt_revenuePrice.setText(priceText);
+        }
+        txt_revenuePercent.setText(mFloatFormatter.format((currentPrice[index] - initialPrice[index]) / initialPrice[index] * 100));
+        if (currentPrice[index] - initialPrice[index] > 0.0d) {
+            txt_currentPrice.setTextColor(Const.Color.LTRED);
+            txt_revenuePrice.setTextColor(Const.Color.LTRED);
+            txt_revenuePercent.setTextColor(Const.Color.LTRED);
+        } else if (currentPrice[index] - initialPrice[index] < 0.0d) {
+            txt_currentPrice.setTextColor(Const.Color.LTBLUE);
+            txt_revenuePrice.setTextColor(Const.Color.LTBLUE);
+            txt_revenuePercent.setTextColor(Const.Color.LTBLUE);
+        }
     }
 }
