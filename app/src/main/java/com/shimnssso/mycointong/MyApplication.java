8d4a4a3e606ace16c4a1adbeb56d09578ebb08c6
sshim from desktop <mechurak@gmail.com>
2017-10-22 00:54:34 +0900

    Comment out Stetho for release build

--- app/src/main/java/com/shimnssso/mycointong/MyApplication.java
+++ app/src/main/java/com/shimnssso/mycointong/MyApplication.java
@@ -2,13 +2,13 @@ package com.shimnssso.mycointong;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
+//import com.facebook.stetho.Stetho;
 
 public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
         // (url) chrome://inspect
-        Stetho.initializeWithDefaults(this);
+//        Stetho.initializeWithDefaults(this);
     }
 }
